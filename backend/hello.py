e50f90605842aff9416de68e304c30dfde74c945
Abhimanyu <abhi@manyu.in>
2020-08-30 20:36:51 +0100

    Update backend/hello.py to return JSON
    This changes hello.py to return a proper JSON object,.

--- backend/hello.py
+++ backend/hello.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from typing import Dict
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world() -> str:
-    return "Hello, World!"
+def index() -> Dict:
+    return {
+        "status": True
+    }
+
+@app.route('/hello')
+def hello() -> Dict:
+    return {
+        "status": True,
+        "data": "Hello, World",
+    }
